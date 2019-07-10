e51603f9682afee45509b661d7f5de9ec8dd5547
Maxim Matyunin <matyunya@gmail.com>
2019-07-10 22:58:37 +0900

    optimize and inline all images

--- index.js
+++ index.js
@@ -4,15 +4,25 @@ const path = require("path");
 const fs = require("fs");
 
 const defaults = {
+  optimizeAll: true,
+  inlineBelow: 10000,
+  compressionLevel: 8,
+  quality: 70,
   tagName: "Image",
   sizes: [400, 800, 1200],
   breakpoints: [375, 768, 1024],
   outputDir: "g/"
 };
 
-async function getBase64(pathname) {
+async function getBase64(pathname, inlined = false) {
+  let size = 64;
+
+  if (inlined) {
+    size = await sharp(pathname).metadata();
+  }
+
   const s = await sharp(pathname)
-    .resize(96)
+    .resize(size)
     .toBuffer();
 
   return "data:image/png;base64," + s.toString("base64");
@@ -25,21 +35,33 @@ function getProp(node, attr) {
 function getPathname(node) {
   const [value] = getProp(node, "src");
 
-  if (value.type === "MustacheTag") {
+  // dynamic or empty value
+  if (
+    value.type === "MustacheTag" ||
+    value.type === "AttributeShorthand" ||
+    !value.data
+  ) {
     // TODO:
-    // infer import
-    // throw if dynamic
+    // resolve imported path
 
-    return "";
+    throw new Error("Can't process the image");
   }
   return path.resolve("./static/", value.data);
 }
 
-function getFilename(p, size) {
-  return `${size}-${path.basename(p, path.extname(p))}${path.extname(p)}`;
+function getBasename(p) {
+  return path.basename(p, path.extname(p));
 }
 
-function add(content, value, start, end, offset) {
+function getFilename(p) {
+  return path.basename(p);
+}
+
+function getFilenameWithSize(p, size) {
+  return `${getBasename(p)}-${size}${path.extname(p)}`;
+}
+
+function insert(content, value, start, end, offset) {
   return {
     content:
       content.substr(0, start + offset) + value + content.substr(end + offset),
@@ -49,19 +71,18 @@ function add(content, value, start, end, offset) {
 
 function resize(options, pathname) {
   return async size => {
-    const filename = getFilename(pathname, size);
+    const filename = getFilenameWithSize(pathname, size);
 
     const dir = "./static/" + options.outputDir;
-
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir);
-    }
-
     const outPath = path.resolve(dir, filename);
+
+    const meta = await sharp(pathname).metadata();
+
+    if (meta.width < size) return null;
 
     if (fs.existsSync(outPath)) {
       return {
-        ...(await sharp(pathname).metadata()),
+        ...meta,
         filename: options.outputDir + filename,
         size
       };
@@ -69,7 +90,10 @@ function resize(options, pathname) {
 
     return {
       ...(await sharp(pathname)
-        .resize(size)
+        .resize({ width: size, withoutEnlargement: true })
+        .jpeg({ quality: options.quality, progressive: true, force: false })
+        .webp({ quality: options.quality, lossless: true, force: false })
+        .png({ compressionLevel: options.compressionLevel, force: false })
         .toFile(outPath)),
       size,
       filename: options.outputDir + filename
@@ -77,32 +101,83 @@ function resize(options, pathname) {
   };
 }
 
+function init(options) {
+  const dir = "./static/" + options.outputDir;
+
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+}
+
 function getSrcset(sizes, options) {
   const srcSetValue = sizes
+    .filter(f => f)
     .map((s, i) => `${s.filename} ${options.breakpoints[i]}w`)
     .join(",\n");
 
   return `srcset=\'${srcSetValue}\'`;
 }
 
-async function replace(edited, node, options) {
+async function replaceInComponent(edited, node, options) {
   const { content, offset } = await edited;
-  const pathname = getPathname(node);
+  let pathname = "";
+
+  try {
+    pathname = getPathname(node);
+  } catch (e) {
+    return { content, offset };
+  }
+
+  const sizes = await Promise.all(options.sizes.map(resize(options, pathname)));
 
   const base64 = await getBase64(pathname);
   const [{ start, end }] = getProp(node, "src");
 
-  const withBase64 = add(content, base64, start, end, offset);
+  const withBase64 = insert(content, base64, start, end, offset);
 
-  const sizes = await Promise.all(options.sizes.map(resize(options, pathname)));
-
-  return add(
+  return insert(
     withBase64.content,
     getSrcset(sizes, options),
     end + 1,
     end + 2,
     withBase64.offset
   );
+}
+
+async function optimize(p, options) {
+  const inPath = path.resolve("./static/", getFilename(p));
+  const outPath = path.resolve("./static/", options.outputDir, getFilename(p));
+  const outUrl = options.outputDir + getFilename(p);
+
+  const { size } = fs.statSync(inPath);
+  if (options.inlineBelow && size < options.inlineBelow) {
+    return getBase64(inPath, true);
+  }
+
+  await sharp(inPath)
+    .jpeg({ quality: options.quality, progressive: true, force: false })
+    .webp({ quality: options.quality, lossless: true, force: false })
+    .png({ compressionLevel: options.compressionLevel, force: false })
+    .toFile(outPath);
+
+  return outUrl;
+}
+
+async function replaceInImg(edited, node, options) {
+  const { content, offset } = await edited;
+  let p = "";
+
+  try {
+    p = getPathname(node);
+  } catch (e) {
+    return { content, offset };
+  }
+
+  const [{ start, end }] = getProp(node, "src");
+
+  const outUrl = await optimize(p, options);
+
+  return insert(content, outUrl, start, end, offset);
 }
 
 async function replaceImages(content, options) {
@@ -115,8 +190,12 @@ async function replaceImages(content, options) {
 
   svelte.walk(ast, {
     enter: node => {
-      if (node.name !== options.tagName) return;
+      if (options.optimizeAll && node.name === "img") {
+        imageNodes.push(node);
+        return;
+      }
 
+      if (node.name !== options.tagName) return;
       imageNodes.push(node);
     }
   });
@@ -128,10 +207,12 @@ async function replaceImages(content, options) {
     offset: 0
   };
 
-  const processed = await imageNodes.reduce(
-    async (edited, node) => replace(edited, node, options),
-    beforeProcessed
-  );
+  const processed = await imageNodes.reduce(async (edited, node) => {
+    if (node.name === "img") {
+      return replaceInImg(edited, node, options);
+    }
+    return replaceInComponent(edited, node, options);
+  }, beforeProcessed);
 
   return processed.content;
 }
@@ -141,6 +222,8 @@ module.exports = function getPreprocessor(options = defaults) {
     ...defaults,
     ...options
   };
+
+  init(options);
 
   return {
     markup: async ({ content }) => ({
