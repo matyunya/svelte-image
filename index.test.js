9bbd9bab6df74acdc97cdf65b95478a6451b97ee
Don Denton <don@happycollision.com>
2019-10-27 23:51:44 -0500

    Split files for easier testing
    This will allow us to export anything we'd like from the main.js file, while reserving the index file to export ONLY public methods.

---
+++ index.test.js
@@ -0,0 +1,18 @@
+const getPreprocessor = require('./index')
+
+afterEach(() => jest.resetAllMocks())
+
+describe('the main export', () => {
+  test('the module returns a function', () => {
+    expect(() =>getPreprocessor()).not.toThrow()
+    expect(typeof getPreprocessor().markup).toEqual('function')
+  })
+  
+  test('it does fine with basic markup', async () => {
+    const {markup} = getPreprocessor()
+    const content = `<p>It works.</p>`
+    
+    const {code} = await markup({content: content})
+    expect(code).toEqual(`<p>It works.</p>`)
+  })
+})
